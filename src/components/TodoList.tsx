3879d63ef2cca1bb65da1dd30e5515bb042deb1c
Danya <danil-romanyuk-2000@mail.ru>
2022-06-09 20:17:52 +0300

    Configure eslint

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,37 +1,36 @@
-import React from "react";
+import React from 'react'
 import styles from './TodoList.module.css'
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import DeleteIcon from '@mui/icons-material/Delete';
-
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import DeleteIcon from '@mui/icons-material/Delete'
 
 const TodoList = () => {
-    return(
+  return (
         <div className={styles.TodoList}>
             <div className={styles.TodoList__container}>
             <h1 className={styles.TodoList__title}>TodoList</h1>
             <div className={styles.TodoList__form}>
-                <TextField  
+                <TextField
                     label="Todo"
                     id="fullWidth"
-                    size="small" 
+                    size="small"
                     className={styles.TodoList__todoInput}/>
-                <Button 
+                <Button
                     variant="outlined"
                     className={styles.TodoList__addTodoBtn}>Add Todo</Button>
-                <Button 
+                <Button
                     variant="outlined"
-                    color="error" 
+                    color="error"
                     className={styles.TodoList__clearTodosBtn}
                     >Clear Todos
                     <DeleteIcon color="error" />
                     </Button>
             </div>
-           
+
             </div>
-            
+
         </div>
-    )
+  )
 }
 
-export default TodoList;
+export default TodoList
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-import './index.css';
+import './index.css'
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+const container = document.getElementById('root')!
+const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
@@ -13,6 +13,4 @@ root.render(
       <App />
 
   </React.StrictMode>
-);
-
-
+)
