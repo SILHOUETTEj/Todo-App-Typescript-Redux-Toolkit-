0844a04a4534709234b0554d00cea6923f10e3b9
Danya <danil-romanyuk-2000@mail.ru>
2022-06-16 14:02:02 +0300

    adaptive

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,18 +1,22 @@
 import DeleteIcon from "@mui/icons-material/Delete";
-import {Button, Checkbox} from "@mui/material";
-import {TodoItem} from "../store/todolist/todoSlice";
+import { Button, Checkbox } from "@mui/material";
+import { TodoItem } from "../store/todolist/todoSlice";
 import styles from "./TodoItem.module.css";
 
 type TodoItemProps = {
   data: TodoItem;
 };
-const TodoItemComponent = ({data}: TodoItemProps) => {
+const TodoItemComponent = ({ data }: TodoItemProps) => {
   return (
     <li className={styles.TodoItem}>
       <div className={styles.TodoItem__container}>
         <div className={styles.TodoItem__text}>{data.text}</div>
-        <Checkbox />
-        <Button variant="outlined" startIcon={<DeleteIcon />}>
+        <Checkbox className={styles.TodoItem__checkbox} />
+        <Button
+          className={styles.TodoItem__deleteTodoBtn}
+          variant="outlined"
+          startIcon={<DeleteIcon />}
+        >
           Delete
         </Button>
       </div>
